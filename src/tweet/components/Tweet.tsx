7fb527fe57005bb93d9e36916d4fec21573ea2e1
jhonson-lc <jhoncisneros11@gmail.com>
2022-09-02 09:31:18 -0500

    Add types of avatar

--- src/tweet/components/Tweet.tsx
+++ src/tweet/components/Tweet.tsx
@@ -1,37 +1,64 @@
-import { Tweet } from '@/tweet/types';
-import { Title,Sub,Content } from '@/ui/feedback/Text';
-import { Stack,Avatar,IconButton, HStack, Box } from '@chakra-ui/react';
-import React from 'react';
-import { FiMoreHorizontal } from 'react-icons/fi';
+import { Tweet } from "@/tweet/types";
+import { Title, Sub, Content } from "@/ui/feedback/Text";
+import { Stack, Avatar, IconButton, HStack } from "@chakra-ui/react";
+import React from "react";
+import { FiMoreHorizontal } from "react-icons/fi";
 
-const Tweet: React.FC<Tweet> = ({author,user,content,updatedAt}) => {
-
-    return (
-        <Stack p={2} w="full" borderBottom="1px solid #fafafa30" direction="row">
-            <Avatar/>
-            <Stack w='full' spacing={0} justifyContent={"start"}>
-                <HStack align={"center"} justifyContent="space-between">
-                    <Title label={author}/>
-                    <HStack justify="start">
-                        <Sub label={user}/>
-                        <span>·</span>
-                        <Sub label={updatedAt}/>
-                    </HStack>
-                    <Stack alignItems={"end"} flex={1}><FiMoreHorizontal/></Stack>
-                </HStack>
-                <HStack alignItems="start">
-                    <Content label={content}/>
-                </HStack>
-                <HStack justify="space-between">
-                    <IconButton variant="ghost" icon={<FiMoreHorizontal/>} aria-label='Settings'/>
-                    <IconButton variant="ghost" icon={<FiMoreHorizontal/>} aria-label='Settings'/>
-                    <IconButton variant="ghost" icon={<FiMoreHorizontal/>} aria-label='Settings'/>
-                    <IconButton variant="ghost" icon={<FiMoreHorizontal/>} aria-label='Settings'/>
-                    <IconButton variant="ghost" icon={<FiMoreHorizontal/>} aria-label='Settings'/>
-                </HStack>
-            </Stack>
-        </Stack>
-    );
-}
+const Tweet: React.FC<Tweet> = ({
+  author,
+  username,
+  image,
+  content,
+  updatedAt,
+}) => {
+  return (
+    <Stack p={2} w="full" borderBottom="1px solid #fafafa30" direction="row">
+      <Avatar src={image} />
+      <Stack w="full" spacing={0} justifyContent={"start"}>
+        <HStack align={"end"} justifyContent="space-between">
+          <Title fontWeight={600} variant="tweet" label={author} />
+          <HStack>
+            <Sub label={username} />
+            <Sub label="·" />
+            <Sub label={updatedAt} />
+          </HStack>
+          <Stack alignItems={"end"} flex={1}>
+            <FiMoreHorizontal />
+          </Stack>
+        </HStack>
+        <HStack alignItems="start">
+          <Content label={content} />
+        </HStack>
+        <HStack justify="space-between">
+          <IconButton
+            variant="ghost"
+            icon={<FiMoreHorizontal />}
+            aria-label="Settings"
+          />
+          <IconButton
+            variant="ghost"
+            icon={<FiMoreHorizontal />}
+            aria-label="Settings"
+          />
+          <IconButton
+            variant="ghost"
+            icon={<FiMoreHorizontal />}
+            aria-label="Settings"
+          />
+          <IconButton
+            variant="ghost"
+            icon={<FiMoreHorizontal />}
+            aria-label="Settings"
+          />
+          <IconButton
+            variant="ghost"
+            icon={<FiMoreHorizontal />}
+            aria-label="Settings"
+          />
+        </HStack>
+      </Stack>
+    </Stack>
+  );
+};
 
 export default Tweet;
--- src/tweet/forms/TweetsForm/TweetsForm.tsx
+++ src/tweet/forms/TweetsForm/TweetsForm.tsx
@@ -1,39 +1,78 @@
-import Button from '@/components/Button';
-import { Avatar, Divider, HStack, IconButton, Input, Select, Stack, Textarea } from '@chakra-ui/react';
-import React, { SetStateAction } from 'react';
-import { FiMoreHorizontal } from 'react-icons/fi';
+import Button from "@/components/Button";
+import {
+  Avatar,
+  Divider,
+  HStack,
+  IconButton,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import React from "react";
+import { FiMoreHorizontal } from "react-icons/fi";
 
 interface Props {
-    set: any;
-    tweet:string;
-    click: ()=>void;
+  set: any;
+  tweet: string;
+  click: () => void;
 }
 
-const TweetsForm: React.FC<Props> = ({ set,tweet,click }) => {
-
-    return (
-        <Stack w="full" direction="row" p={2} borderBottom="1px solid #fafafa30">
-            <Avatar />
-                <Stack w="full">
-                    <Stack>
-                        <form>
-                            <Input onChange={(e)=>set(e.target.value)} value={tweet} fontSize={20} variant="unstyled" placeholder="What's happening?" />
-                        </form>
-                    </Stack>
-                    <Divider borderColor="#fafafa30" />
-                    <Stack direction="row" justifyContent={"space-between"}>
-                        <HStack>
-                            <IconButton variant="ghost" icon={<FiMoreHorizontal />} aria-label='Settings' />
-                            <IconButton variant="ghost" icon={<FiMoreHorizontal />} aria-label='Settings' />
-                            <IconButton variant="ghost" icon={<FiMoreHorizontal />} aria-label='Settings' />
-                            <IconButton variant="ghost" icon={<FiMoreHorizontal />} aria-label='Settings' />
-                            <IconButton variant="ghost" icon={<FiMoreHorizontal />} aria-label='Settings' />
-                        </HStack>
-                        <Button onclick={click} type="submit" label="Tweet" />
-                    </Stack>
-                </Stack>
+const TweetsForm: React.FC<Props> = ({ set, tweet, click }) => {
+  const { data: session } = useSession();
+  return (
+    <Stack w="full" direction="row" p={2} borderBottom="1px solid #fafafa30">
+      <Avatar src={session.user.image} />
+      <Stack w="full">
+        <Stack>
+          <form>
+            <Input
+              onChange={(e) => set(e.target.value)}
+              value={tweet}
+              fontSize={20}
+              variant="unstyled"
+              placeholder="What's happening?"
+            />
+          </form>
         </Stack>
-    );
-}
+        <Divider borderColor="#fafafa30" />
+        <Stack direction="row" justifyContent={"space-between"}>
+          <HStack>
+            <IconButton
+              variant="ghost"
+              icon={<FiMoreHorizontal />}
+              aria-label="Settings"
+            />
+            <IconButton
+              variant="ghost"
+              icon={<FiMoreHorizontal />}
+              aria-label="Settings"
+            />
+            <IconButton
+              variant="ghost"
+              icon={<FiMoreHorizontal />}
+              aria-label="Settings"
+            />
+            <IconButton
+              variant="ghost"
+              icon={<FiMoreHorizontal />}
+              aria-label="Settings"
+            />
+            <IconButton
+              variant="ghost"
+              icon={<FiMoreHorizontal />}
+              aria-label="Settings"
+            />
+          </HStack>
+          <Button
+            disabled={tweet === "" ? true : false}
+            onclick={click}
+            type="submit"
+            label="Tweet"
+          />
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
 
 export default TweetsForm;
